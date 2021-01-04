86c0f4beb51462da811d0891717da5095085e6a5
PierreMazurek <57002827+PierreMazurek@users.noreply.github.com>
2021-01-04 14:13:16 +0100

    Tests sur la classe City

--- src/test/java/tsp/lee/jacobson/CityTest.java
+++ src/test/java/tsp/lee/jacobson/CityTest.java
@@ -4,6 +4,10 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @Pierre_Mazurek
+ */
+
 public class CityTest {
 
 	@Test
@@ -11,4 +15,43 @@ public class CityTest {
 		fail("Not yet implemented");
 	}
 
+	City A = new City(0,0);
+	City B = new City(0,2);
+	City C = new City(2,0);
+
+
+	@Test
+	public void distanceTo_AFromB_2() {
+		double expected = 2;
+		double actual = B.distanceTo(A);
+		assertEquals(expected, actual, 0.0);
+	}
+
+	@Test
+	public void distanceTo_AFromC_2() {
+		double expected = 2;
+		double actual = C.distanceTo(A);
+		assertEquals(expected, actual, 0.0);
+	}
+
+	@Test
+	public void distanceTo_AFromA_0() {
+		double expected = 0;
+		double actual = A.distanceTo(A);
+		assertEquals(expected, actual, 0.0);
+	}
+
+	@Test
+	public void distanceTo_DFromE_5() {
+		City D = new City(0,5);
+		City E = new City(0, 0);
+		double expected = 5;
+		double actual = D.distanceTo(E);
+		assertEquals(expected, actual, 0.0);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void distanceTo_Null_NullPointerException() {
+		double actual = A.distanceTo(null);
+	}
 }
